c8168fd617f18e6b7bb849c6a734c85983761341
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-04-24 13:45:38 +0600

    restart project

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Shah Md Imran Hossain on 24/4/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Shah Md Imran Hossain on 24/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
